16a7b0d9a55f8f999cbc16b3ed8332ec19a284e1
laurakalbag <me@laurakalbag.com>
2025-12-02 13:42:29 +0000

    Tweak copy in update text

--- UpdateText.fragment.js
+++ UpdateText.fragment.js
@@ -9,5 +9,5 @@ import kitten from '@small-web/kitten'
 
 export default () => kitten.html`
     <p>We first published Accessibility For Everyone with <a href="https://abookapart.com/blogs/press/a-new-chapter-for-a-book-apart">A Book Apart</a> back in 2017. This means the book is now ${new Date().getFullYear()-2017} years old. Accessibility best practices haven’t really changed, and you should still find this book valuable. However, some of the recommended tools might be outdated, job titles have changed, and the <a href="https://www.w3.org/WAI/standards-guidelines/wcag/wcag3-intro/">Web Content Accessibility Guidelines are now approaching version 3</a>.</p>
-    <p>Perhaps someday I (Laura) will find time to write a second edition, but first I wanted to make the book freely available for the web community who have generously shared their knowledge with me.</p>
+    <p>Perhaps someday I (Laura) will find time to write a second edition, but first I wanted to make the book free, and available for the web community who have generously shared their knowledge with me.</p>
 `
