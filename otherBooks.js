2780cc2ffea889b2b6f1303e158a5f4e0c031b82
laurakalbag <me@laurakalbag.com>
2026-01-26 16:23:05 +0000

    Add a few more authors

--- otherBooks.js
+++ otherBooks.js
@@ -12,6 +12,21 @@ export default () => kitten.html`
       <p>Find other books from the Author Collective (authors previously published by A Book Apart) from their websites below:</p>
       <ul>
         <li>
+          <a href='https://www.bethdunn.com/book'>Cultivating Content Design</a> by Beth Dunn
+        </li>
+        <li>
+          <a href='https://everydayia.com'>Everyday Information Architecture</a> by Lisa Maria Marquis
+        </li>
+        <li>
+          <a href='https://practical-svg.chriscoyier.net'>Practical SVG</a> by Chris Coyier
+        </li>
+        <li>
+          <a href='http://www.onwardand.co/seo-for-everyone'>SEO for Everyone</a> by Rebekah Baggs and Chris Corak
+        </li>
+        <li>
+          <a href='https://balsamiq.com/learn/wireframing-book/'>Wireframing For Everyone</a> by Michael Angeles, Leon Barnard, and Billy Carlson
+        </li>
+        <li>
           <a href='https://ethanmarcotte.com/books/you-deserve-a-tech-union/'>You Deserve a Tech Union</a> by Ethan Marcotte
         </li>
       </ul>
